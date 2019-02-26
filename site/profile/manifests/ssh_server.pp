4dc17f9950dfe4c51fce69bd298af34e71442be2
Alan <TeamWaggles@users.noreply.github.com>
2019-02-25 19:49:03 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDRlCv0PJcqY13xRloAPLdja1ApgNPi+TwMx/BXt+9uIzaBxlt8ZMmPOtVOPOvp3QKs/Ln4XklF4ztT7jTGWuIClKenCoA40pivU0f+AhU/c7Ungy6HI6KPZlbqS1qxgfJ5U8W0bq7GbfkZgfEddXNwoS7TVQRb/dkCyzmw1rNCs2YGV//zWttgmLUDOwteT5OSDk/r482BPU5JfbcssnbVwRX0IrcNgfHjCcm1R6GVIgm4EixScgiNanIFF9INJGKWwCmhznXL+QdIc8dnZge978TQpM8zdL2CPtT0kTmavhmX7Nq31/NbhwfyOv1//TP3BYEYJk/envIpieltfjhj',
 	}  
 }
